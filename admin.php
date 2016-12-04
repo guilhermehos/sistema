09f4c74f5e9a2dc91abec361a705ff90e0515e2f
Guilhermehos <guilherme.souza19@outlook.com.br>
2016-12-04 14:11:20 -0200

    pagina de admin

--- admin.php
+++ admin.php
@@ -14,7 +14,7 @@ $idrec = $_SESSION['idusuario']; // variavel para salvamento automatico de quem 
 
 if (isset($_GET["action"]) AND $_GET["action"] == "sair") {
   session_destroy();
-  header ("Location://localhost/sistema/index.php");
+  header ("Location: http://localhost/sistema/index.php");
 }
 
 ?>
@@ -24,36 +24,43 @@ if (isset($_GET["action"]) AND $_GET["action"] == "sair") {
 </head>
 
 <body>
-<div class="col-xs-12">
-  <div class="row">
-    <div class="container">
-    <a href="?action=sair"> <button class="btn btn-danger pull-right col-xs-2">  Sair  </button> </a>
-      <img src="css/imagens/logo_topo.png" class="img-responsive" />
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <a href="?action=sair"> <button class="btn btn-danger pull-right col-lg-1">  Sair  </button> </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <img src="css/imagens/logo_topo.png" class="img-responsive" />
         <div class="col-xs-8">
-        <img src="css/imagens/adm.png" class="img-responsive" />
+          <img src="css/imagens/adm.png" class="img-responsive" />
         </div>
 
-               <table class="table table-striped table-bordered table-hover table-condensed table-responsive">
-                <thead>
-                  <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Nivel</th>
-                        <th>Status</th>
-                        <th>Ação</th>
-                  </tr>
-              </thead>
-              <tbody>
+        <table class="table table-hover table-condensed table-responsive">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Nivel</th>
+              <th>Status</th>
+              <th>Ação</th>
+            </tr>
+          </thead>
+          <tbody>
 
-<?php
+            <?php
 // Conecta ao banco de dados
-include ("config.php");
+            include ("config.php");
 
 $num_registro = 10; // número de registro que quero que apareça por página
 $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 0; // aqui eu pego da url o número da página que estou.
 if(empty($pagina))
 {
-$pagina = 1;
+  $pagina = 1;
 }
 $inicio = ($pagina * $num_registro) - $num_registro;  //cálculo para saber em que registro a query deve iniciar sua contagem, pego a página que estou multiplico pelo número de registros que quero que apareça no máximo na página e subtraio por esse mesmo número.
 
@@ -67,34 +74,34 @@ if($numCont > 0)
 { 
 for($x=0; $x<$numCont; $x++) // faço um for até o número máximo de registros que quero que apareça na página
 {
-$dados = $sqlCont->fetch_array();
+  $dados = $sqlCont->fetch_array();
 
 
-?>
-      
+  ?>
+  
 
-                   <!-- Exibir usuários com a função de bloquear e aprovar -->     
-                  <tr>
-                      <td><?php echo $dados['ID'];?></td>
-                      <td><?php echo $dados['nome'];?></td>
-                      <td><?php echo $dados['nivel'];?></td>
-                      <td><?php echo $dados['status'];?></td>
-                      <td><?php if($dados['status'] == 0);?>
-                          <a href="panel/status_aprovar.php?id=<?php echo $dados["ID"];?>"> Aprovar |</a>
-                          <?php if($dados['status'] == 1);?>
-                          <a href="panel/status_bloquear.php?id=<?php echo $dados["ID"];?>"> Bloquear</a>
-                      </td>                             
-                  </tr>   
+  <!-- Exibir usuários com a função de bloquear e aprovar -->     
+  <tr>
+    <td><?php echo $dados['ID'];?></td>
+    <td><?php echo $dados['nome'];?></td>
+    <td><?php echo $dados['nivel'];?></td>
+    <td><?php echo $dados['status'];?></td>
+    <td><?php if($dados['status'] == 0);?>
+      <a href="panel/status_aprovar.php?id=<?php echo $dados["ID"];?>"> Aprovar |</a>
+      <?php if($dados['status'] == 1);?>
+      <a href="panel/status_bloquear.php?id=<?php echo $dados["ID"];?>"> Bloquear</a>
+    </td>                             
+  </tr>   
 
-                               
-                                          
-                  
-<?php
+  
+  
+  
+  <?php
 }
 }
 else
 {
-echo "<h3>Nenhum Contato registrado.</h3>";
+  echo "<h3>Nenhum Contato registrado.</h3>";
 }
 ?> 
 </tbody>
@@ -104,28 +111,28 @@ echo "<h3>Nenhum Contato registrado.</h3>";
  <?php
 
 //A partir daqui para baixo eu faço os links da paginação, anterior, próximo, último, etc, que serve para você navegar pela paginação e acessar os registros.
-if($numCont > 0)
-{
-if($numTotal>$num_registro)
-{
-$totalPag = Ceil($numTotal / $num_registro);
-echo '<a href="admin.php?pagina=1"> Primeiro </a>'; 
-for($i=1; $i <= $totalPag; $i++)
-{
-if($pagina == $i)
-{
-echo "<span>&nbsp; ".$i." &nbsp;<span>";
-}
-else
-{
-$indice = "&pagina=".$i;
-echo '<a href="admin.php?'.$indice.'">&nbsp; '.$i.' &nbsp;</a>';
+ if($numCont > 0)
+ {
+  if($numTotal>$num_registro)
+  {
+    $totalPag = Ceil($numTotal / $num_registro);
+    echo '<a href="admin.php?pagina=1"> Primeiro </a>'; 
+    for($i=1; $i <= $totalPag; $i++)
+    {
+      if($pagina == $i)
+      {
+        echo "<span>&nbsp; ".$i." &nbsp;<span>";
+      }
+      else
+      {
+        $indice = "&pagina=".$i;
+        echo '<a href="admin.php?'.$indice.'">&nbsp; '.$i.' &nbsp;</a>';
 
-}
-}
-echo '<a href="admin.php?pagina='.$totalPag.'">&nbsp; Último &nbsp;</a>';
+      }
+    }
+    echo '<a href="admin.php?pagina='.$totalPag.'">&nbsp; Último &nbsp;</a>';
 
-} 
+  } 
 } 
 ?>
 </div>
